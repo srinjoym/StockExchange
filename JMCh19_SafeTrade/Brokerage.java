6c5e5ec76d055028fc2c1d7cf1bcb4cf54c99f48
Edward <Edward@Computer.attlocal.net>
2015-03-21 11:57:35 -0700

    Finished Brokerage constructor and addUser method

--- JMCh19_SafeTrade/Brokerage.java
+++ JMCh19_SafeTrade/Brokerage.java
@@ -1,24 +1,58 @@
 import java.lang.reflect.*;
 import java.util.*;
 
-/**edward rulesfda
- * Represents a brokeragXZe.asdf
+
+/**
+ * edward rulesfda Represents a brokeragXZe.asdf
  */
 public class Brokerage implements Login
 {
     private Map<String, Trader> traders;
+
     private Set<Trader> loggedTraders;
+
     private StockExchange exchange;
+
 
     // TODO complete class
 
-
-    public Brokerage (StockExchange ex)
+    public Brokerage( StockExchange ex )
     {
         exchange = ex;
-        TreeMap<String,Trader> tree = new TreeMap<String,Trader>();
+        TreeMap<String, Trader> tree = new TreeMap<String, Trader>();
         tree.putAll( traders );
+        loggedTraders = new TreeSet<Trader>();
     }
+
+
+    @Override
+    public int addUser( String name, String password )
+    {
+        if ( name.length() < 11 && name.length() > 3 )
+        {
+
+            if ( password.length() > 1 && password.length() < 11 )
+            {
+                if ( !traders.containsKey( name ) )
+                {
+                    return 0;
+                }
+                return 3;
+            }
+            return 2;
+        }
+        return 1;
+    }
+
+
+    @Override
+    public int login( String name, String password )
+    {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+
     //
     // The following are for test purposes only
     //
@@ -27,15 +61,18 @@ public class Brokerage implements Login
         return traders;
     }
 
+
     protected Set<Trader> getLoggedTraders()
     {
         return loggedTraders;
     }
 
+
     protected StockExchange getExchange()
     {
         return exchange;
     }
+
 
     /**
      * <p>
@@ -71,17 +108,4 @@ public class Brokerage implements Login
         return str + "]";
     }
 
-    @Override
-    public int addUser( String name, String password )
-    {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-    @Override
-    public int login( String name, String password )
-    {
-        // TODO Auto-generated method stub
-        return 0;
-    }
 }
